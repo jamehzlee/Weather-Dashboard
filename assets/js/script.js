5a286a31432c09dadb1c3ecc43e97b0b59940b8e
Jamehzz <probableeme@gmail.com>
2022-03-06 19:22:03 -0500

    added 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,93 @@
-let apiData = 0;
-let citySearch = "New York";
+// key 1  90a5b2cd046d7e3c1493dd6299106c4d
+// key 2  b0032ccf1f942a837562c29b20858d84
+// key 3  947940214a5feca7c6125ce4a4937d43
+
+let searchBtnEl = document.querySelector("#search-btn")
+let userInput = document.querySelector("#user-city")
 let currentDayEl = document.querySelector("#current")
 let fiveDayEl = document.querySelector("#five-day")
+let fiveCardEl = fiveDayEl.children
+let time = new Date()
+let utcTime = 0
+let cityName = ""
+let uv = 0
+let temp = 0
+let wind = 0
+let humid = 0
+let month = 0
+let day = 0
+let year = 0
+let lat = 0
+let lon = 0
+let test = "los angeles"
 
-function getCurrentDay() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+citySearch+"&appid=fa6a5bb676fc0a7edc88cf13590ef043")
+function searchCity() {
+    let userCity = userInput.value
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+userCity+"&limit=1&appid=947940214a5feca7c6125ce4a4937d43")
     .then(header => header.json())
     .then(response => {
-        apiData = response
-        console.log(apiData);
-        toFaren(apiData.main.temp)
-        console.log(faren);
+        console.log(response);
+        lat = response[0].lat
+        lon = response[0].lon
+        cityName = response.name
+        // getCurrentDay()
+        getFiveDay()
+    })
+}
+
+function getCurrentDay() {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&exclude=minutely,hourly,daily,alerts&appid=947940214a5feca7c6125ce4a4937d43")
+    .then(header => header.json())
+    .then(response => {
+        utcTime = response.current.dt
+        temp = response.current.temp
+        wind = response.current.wind_speed
+        humid = response.current.humidity
+        uv = response.current.uvi
+        editCurrent()
+        console.log(response);
     })
 }
 
 function getFiveDay() {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=orlando&appid=fa6a5bb676fc0a7edc88cf13590ef043")
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&exclude=current,minutely,hourly,alerts&appid=947940214a5feca7c6125ce4a4937d43")
     .then(header => header.json())
     .then(response => {
-        apiData = response
-        console.log(apiData);
-        toFaren(apiData.list[0].main.temp)
-        console.log(faren);
+        for (let i = 0; i < fiveDayEl.children.length; i++) {
+            utcTime = response.daily[i].dt
+            temp = response.daily[i].temp.day
+            wind = response.daily[i].wind_speed
+            humid = response.daily[i].humidity
+            fiveCardEl = fiveDayEl.children[i]
+            editFive()
+        }
+        console.log(response)
     })
 }
 
-// function displayCurrent() {
-    
-//     getCurrentDay()
-// }
-
-// function displayFive() {
-//     for (let i = 0; i < 5; i++) {
-
-//     }
-//     getFiveDay()
-// }
-
- function toFaren(num) {
-    faren = ((num - 273.15)*1.8)+32
-    faren = faren.toFixed(2)
+function editCurrent() {
+    currentDayEl.children[0].innerText = cityName + " (" + time.toLocaleDateString(utcTime) + ") " + "icon"
+    currentDayEl.children[1].innerText = "Temp : " + temp + " °F"
+    currentDayEl.children[2].innerText = "Wind: " + wind + " MPH"
+    currentDayEl.children[3].innerText = "Humidity: " + humid + " %"
+    currentDayEl.children[4].innerText = "UV Index: " + uv
 }
+
+function editFive() {
+    utcTime = time.toUTCString
+    month = time.getMonth(utcTime) + 1
+    day = time.getDate(utcTime)
+    year = time.getFullYear(utcTime)
+    fiveCardEl.children[0].innerText = month + "/" + day + "/" + year 
+    fiveCardEl.children[1].innerText = "icon"
+    fiveCardEl.children[2].innerText = "Temp : " + temp + " °F"
+    fiveCardEl.children[3].innerText = "Wind: " + wind + " MPH"
+    fiveCardEl.children[4].innerText = "Humidity: " + humid + " %"
+ }
+
+searchBtnEl.addEventListener("click", function() {
+    searchCity()
+})
+// userInput.addEventListener("", function() {
+//  searchCity()    
+// })
